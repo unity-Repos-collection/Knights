2d3a3991c8e17553ece7ca982dd71ed07c5c0d96
Luca Farci <lfarci09@gmail.com>
2022-12-05 20:11:51 +0000

    hail sorted

--- Assets/scripts/makeitsnow.cs
+++ Assets/scripts/makeitsnow.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class makeitsnow : MonoBehaviour
 {
+    [SerializeField] ParticleSystem hail1;
+    [SerializeField] ParticleSystem hail2;
+
+    [SerializeField] ParticleSystem hail3;
+    [SerializeField] ParticleSystem hail4;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,37 @@ public class makeitsnow : MonoBehaviour
     {
         
     }
+     
+     
+
+    
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {   
+            Debug.Log("hailing");
+            makeithail();
+            
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        Debug.Log("stop hailing");
+        stophail();
+    }
+    void makeithail()
+    {
+        hail1.Play();
+        hail2.Play();
+        hail3.Play();
+        hail4.Play();
+    }
+    void stophail()
+    {
+        hail1.Stop();
+        hail2.Stop();
+        hail3.Stop();
+        hail4.Stop();
+    }
 }
+
