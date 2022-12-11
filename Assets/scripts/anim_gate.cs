7871c725d2ce0aa61bb8303117ebf74befaa4451
Luca Farci <lfarci09@gmail.com>
2022-12-11 16:16:51 +0000

    mushroom pickup in progress

--- Assets/scripts/anim_gate.cs
+++ Assets/scripts/anim_gate.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class anim_gate : MonoBehaviour
 {
     Animator anim;
+    
+    BoxCollider boxCollider;
+    
     // Start is called before the first frame update
-
+    public bool isdungeon = false;
     void Awake() 
     {
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
     }
     void Start()
     {
@@ -22,13 +26,21 @@ public class anim_gate : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider collider) 
+    void OnTriggerEnter(Collider other) 
     {   
-        Debug.Log("opening");
-        anim.SetBool("Bopen_gate", true);    
+        if (other.gameObject.CompareTag("Player") && isdungeon)
+        {
+            Debug.Log("opening");
+            boxCollider.enabled = false;
+            anim.SetBool("Bopen_gate", true);    
+        }
     }
+    
+    
+    
     private void OnTriggerExit(Collider other) 
-    {
+    {   
+        boxCollider.enabled = true;
         Debug.Log("closing");
         anim.SetBool("Bopen_gate", false);    
     }
---
+++ Assets/scripts/mushroom_pickup.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mushroom_pickup : MonoBehaviour
+{
+    [SerializeField] float rotatespeed;
+    Vector3 startingposition;
+    [SerializeField] Vector3 movementvector;
+    float movementfactor; 
+    [SerializeField] float Period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get transform of object
+        startingposition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sinwave();
+        spin();
+    }
+
+    void sinwave()
+    {
+        if (Period <= Mathf.Epsilon) {return;}
+        
+        float cycles = Time.time / Period; // growing over time
+        
+        const float tau = Mathf.PI * 2f; // constant value of 6.283
+        float RawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementfactor = (RawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
+        
+        
+        Vector3 offset = movementvector * movementfactor; 
+        transform.position = startingposition + offset;
+    }
+
+    void spin()
+    {
+        transform.Rotate(0,1,0 * rotatespeed * Time.deltaTime);
+    }
+}
